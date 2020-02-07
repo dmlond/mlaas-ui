167d4e8ac877d08266bdbd7d330a785eaa3cabdf
Darin London <darin.london@duke.edu>
2020-02-07 08:39:30 -0500

    server.local.env: create-react-app only allows environment variables prefixed with REACT_APP_ src/config/authconfig.js: provides global configuration tied to process.env.REACT_APP environment variables

---
+++ src/config/authconfig.js
@@ -0,0 +1,7 @@
+var Config = {
+    oauth_base_uri: process.env.REACT_APP_OAUTH_BASE_URI,
+    oauth_client_id: process.env.REACT_APP_OAUTH_CLIENT_ID,
+    auth_service_base_uri: process.env.REACT_APP_AUTH_SERVICE_BASE_URI,
+    auth_service_endpoint: process.env.REACT_APP_AUTH_SERVICE_AUTHENTICATION_ENDPOINT
+};
+export default Config;
