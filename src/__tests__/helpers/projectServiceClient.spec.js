9f000dff8462affd9b1a5c2aa1a71f94d1f1aaff
Darin London <darin.london@duke.edu>
2020-02-20 15:04:56 -0500

    full test of projectServiceClient API

--- src/__tests__/helpers/projectServiceClient.spec.js
+++ src/__tests__/helpers/projectServiceClient.spec.js
@@ -54,43 +54,33 @@ describe('projectServiceClient', () => {
       authHelper.jwt = origAuthHelperJwt;
     });
 
-    function spyProjectServiceClientSend() {
-      beforeEach(() => {
-        projectServiceClient.send = jest.fn();
-        projectServiceClient.send.mockImplementation(origSend);
-      });
-      afterEach(() => {
-        projectServiceClient.send = origSend;
-      });
-    }
-
-    function testProjectServiceApi(expectedSendPath, expectedSendMethod, sendCall, extraPayload, ...sendArguments) {
+    function testProjectServiceApi(expectedSendPath, expectedSendMethod, testedMethod, extraPayload, ...sendArguments) {
       describe('expected Project Service API interaction', () => {
-          let expectedApiSendUrl = `${process.env.REACT_APP_PROXY_URI}${expectedSendPath}`;
-
-          spyProjectServiceClientSend();
-
+          beforeEach(() => {
+            projectServiceClient.send = jest.fn();
+          });
+          afterEach(() => {
+            projectServiceClient.send = origSend;
+          });
+  
           it('is expected to call the correct project-service api url', done => {
+              let expectedApiSendUrl = `${process.env.REACT_APP_PROXY_URI}${expectedSendPath}`;
               let expectedSendPayload = {
                   url: expectedApiSendUrl,
-                  method: expectedSendMethod,
-                  headers: {
-                    Accept: "application/json",
-                    Authorization: fakeJwt
-                  }
+                  method: expectedSendMethod
               };
               if (extraPayload != null) {
                   for (var attrname in extraPayload) {
                     expectedSendPayload[attrname] = extraPayload[attrname];
                   }
               }
-              sendCall(...sendArguments, () => {}, () => {});
+              testedMethod(...sendArguments, handleSuccess, handleFailure);
 
               setImmediate(() => {
                   expect(projectServiceClient.send).toBeCalledWith(
                     expectedSendPayload,
-                    expect.anything(),
-                    expect.anything()
+                    handleSuccess,
+                    handleFailure
                   );
                   done();
               });
@@ -141,7 +131,7 @@ describe('projectServiceClient', () => {
     });
 
     describe('.index', () => {
-      let expectedProjectServicePath = config['projects_endpoint'];
+      let expectedProjectServicePath = config.projects_endpoint;
       let expectedProjectServiceSendMethod = 'get'
 
       function subject(s,f) {
@@ -152,14 +142,49 @@ describe('projectServiceClient', () => {
     });
 
     describe('.create', () => {
-        
+      let expectedProjectServicePath = config.projects_endpoint;
+      let expectedProjectServiceSendMethod = 'post';
+      let newProjectPayload = {
+        name: "newProject",
+        description: "Test Project Creation"
+      };
+      let expectedExtraPayload = {
+        data: newProjectPayload
+      };
+      function subject(p, s,f) {
+          projectServiceClient.create(p, s,f);
+      }
+
+      testProjectServiceApi(expectedProjectServicePath, expectedProjectServiceSendMethod, subject, expectedExtraPayload, newProjectPayload);        
     });
 
     describe('.show', () => {
+      let expectedProjectId = "abc-123-xyz";
+      let expectedProjectServicePath = config.projects_endpoint+"/"+expectedProjectId;
+      let expectedProjectServiceSendMethod = 'get'
 
+      function subject(i, s,f) {
+          projectServiceClient.show(i, s,f);
+      }
+
+      testProjectServiceApi(expectedProjectServicePath, expectedProjectServiceSendMethod, subject, {}, expectedProjectId);
     });
 
     describe('.update', () => {
+      let expectedProjectId = "abc-123-xyz";
+      let expectedProjectServicePath = config.projects_endpoint+"/"+expectedProjectId;
+      let expectedProjectServiceSendMethod = 'put';
+      let updateProjectPayload = {
+        description: "Test Project Creation"
+      };
+      let expectedExtraPayload = {
+        data: updateProjectPayload
+      };
 
+      function subject(i, p, s,f) {
+          projectServiceClient.update(i, p, s,f);
+      }
+
+      testProjectServiceApi(expectedProjectServicePath, expectedProjectServiceSendMethod, subject, expectedExtraPayload, expectedProjectId, updateProjectPayload);
     });
 });
