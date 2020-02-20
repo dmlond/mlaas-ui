5b870af4ef0cd5c86a3f63528118c252ca916044
Darin London <darin.london@duke.edu>
2020-02-20 12:18:12 -0500

    authHelper - implements common getReturnAuthParams method to get urlParams if code exists - login:   - only attempts authentication, does not test if loggedIn is true, or code exists   - does not manipulate location and history on successful authentication authHelper spec tests new login functionality

--- src/__tests__/helpers/authHelper.spec.js
+++ src/__tests__/helpers/authHelper.spec.js
@@ -115,8 +115,11 @@ describe('authHelper', () => {
   describe('.accessCodeExists', () => {
     describe('getOauthCodeFromURI returns null', () => {
       it('should be false', () => {
+        let originalFunction = authHelper.getOauthCodeFromURI;
+        authHelper.getOauthCodeFromURI = jest.fn();
         expect(authHelper.getOauthCodeFromURI()).toBeNull;
         expect(authHelper.accessCodeExists()).toBeFalsy;
+        authHelper.getOauthCodeFromURI = originalFunction;
       });
     });
     describe('getOauthCodeFromURI returns code', () => {
@@ -133,186 +136,110 @@ describe('authHelper', () => {
   describe('.getOauthCodeFromURI', () => {
     describe('url parameter code does not exist', () => {
       it('should be null', () => {
-        expect(window.location.href.indexOf("?code")).not.toBeGreaterThan(0);
+        let fakeAuthParams = {};
+        fakeAuthParams.get = jest.fn((key) => { return null; });
+        let origAuthReturnParams = authHelper.authReturnParams;
+        authHelper.authReturnParams = jest.fn(() => { return fakeAuthParams; });
         expect(authHelper.getOauthCodeFromURI()).toBeNull;
+        expect(fakeAuthParams.get).toHaveBeenCalledWith('code');
+        authHelper.authReturnParams = origAuthReturnParams;
       });
     });
     describe('url parameter code exists', () => {
       it('should return the token', () => {
-        window.history.pushState({}, 'getOauthCodeFromURI success', `/test.html?code=${expectedJwt}`);
-        expect(window.location.href.indexOf("?code")).toBeGreaterThan(0);
+        let fakeAuthParams = {};
+        fakeAuthParams.get = jest.fn((key) => { return expectedJwt; });
+        let origAuthReturnParams = authHelper.authReturnParams;
+        authHelper.authReturnParams = jest.fn(() => { return fakeAuthParams });
+        expect(authHelper.authReturnParams()).toEqual(fakeAuthParams);
+
         let actualOauthCode = authHelper.getOauthCodeFromURI();
         expect(actualOauthCode).not.toBeNull;
         expect(actualOauthCode).toEqual(expectedJwt);
+        expect(fakeAuthParams.get).toHaveBeenCalledWith('code');
+        authHelper.authReturnParams = origAuthReturnParams;
       });
     });
   });
 
   describe('.login', () => {
-    const origIsLoggedInF = authHelper.isLoggedIn;
-    var isLoggedIn = false;
-    const { location } = window;
-    const mockPath = location.pathname;
-    const mockLocation = location.origin+location.pathname;
-    const mockedOauthRedirectUrl = `${config.oauth_base_uri}/authorize?response_type=code&client_id=${config.oauth_client_id}&state=login&redirect_uri=`+mockLocation;
+    const origGetOauthCodeFromURIF = authHelper.getOauthCodeFromURI;
+    const origAuthClientAuthenticateF = authHelper.authenticate;
     const jwtToken = 'abc123xyz';
     const expiration = 'future';
     const username = 'sheila'
     const timeToLive = '1000';
-
+    var expectedCode;
+    var jwtIsValid;
+    const invalidTokenMessage = "token invalid";
     beforeEach(() => {
-      authHelper.isLoggedIn = jest.fn();
-      authHelper.isLoggedIn.mockImplementation(() => {
-        return isLoggedIn;
+      authHelper.getOauthCodeFromURI = jest.fn(() => { return expectedCode; });
+      authClient.authenticate = jest.fn((c,r,s,f) => {
+        if(jwtIsValid) {
+          s(jwtToken, username, expiration, timeToLive);
+        }
+        else {
+          f(invalidTokenMessage);
+        }
       });
-      delete window.location;
-      //https://remarkablemark.org/blog/2018/11/17/mock-window-location/
-      window.location = { 
-        origin: mockLocation,
-        pathname: mockPath,
-        href: mockLocation,
-        assign: jest.fn(),
-        replace: jest.fn()
-      };
     });
     afterEach(() => {
-      authHelper.isLoggedIn = origIsLoggedInF;
-      window.locaton = location;
+      authHelper.getOauthCodeFromURI = origGetOauthCodeFromURIF;
+      authClient.authenticate = origAuthClientAuthenticateF;
     });
 
-    describe('already logged in', () => {
+    describe('with a valid code', () => {
       it('should call the success handler with true', done => {
-        isLoggedIn = true;
+        jwtIsValid = true;
+        var origJwt = authHelper.jwt;
+        authHelper.jwt = jest.fn(() => {
+          return jwtToken;
+        });
+        authHelper.login().then(
+          handleSuccess,
+          handleFailure
+        );
+        authHelper.jwt = origJwt;
+        setImmediate(() => {
+          expect(handleSuccess).toBeCalledWith(
+            true
+          );
+          expect(authClient.authenticate).toBeCalledWith(
+            expectedCode,
+            window.location.origin+'/login',
+            expect.anything(),
+            expect.anything()
+          )
+          done();
+        });
+      });
+
+      it('should store the jwtToken and expiration in the sessionStorage', done => {
+        jwtIsValid = true;
         authHelper.login().then(
           handleSuccess,
           handleFailure
         );
         setImmediate(() => {
-          expect(handleSuccess).toBeCalledWith(
-            true
-          );
+          expect(sessionStorage.setItem).toHaveBeenCalledWith(expectedJwtStoreKey, jwtToken);
+          expect(sessionStorage.setItem).toHaveBeenCalledWith(expectedJwtExpirationStoreKey, expect.any(Number));
           done();
         });
       });
     });
-
-    describe('not already logged in', () => {
-      const origCodeExistsF = authHelper.accessCodeExists;
-      const origGetOauthCodeFromURIF = authHelper.getOauthCodeFromURI;
-      const origAuthClientAuthenticateF = authClient.authenticate;
-      var expectedCode;
-      var codeExists;
-      var jwtIsValid;
-      const invalidTokenMessage = "token invalid";
-
-      beforeEach(() => {
-        isLoggedIn = false;
-        authHelper.accessCodeExists = jest.fn();
-        authHelper.accessCodeExists.mockImplementation(() => {
-          return codeExists;
-        });
-
-        authHelper.getOauthCodeFromURI = jest.fn();
-        authHelper.getOauthCodeFromURI.mockImplementation(() => {
-          return expectedCode;
-        });
-        authClient.authenticate = jest.fn();
-        authClient.authenticate.mockImplementation(
-          (c,r,s,f) => {
-            if(jwtIsValid) {
-              s(jwtToken, username, expiration, timeToLive);
-            }
-            else {
-              f(invalidTokenMessage);
-            }
-          });
-      });
-      afterEach(() => {
-        authHelper.accessCodeExists = origCodeExistsF;
-        authHelper.getOauthCodeFromURI = origGetOauthCodeFromURIF;
-        authClient.authenticate = origAuthClientAuthenticateF;
-      });
-      describe('access code exists', () => {
-        expectedCode = 'abc123xyz';
-        beforeEach(() => {
-          codeExists = true;
-        });
-
-        describe('and is a valid jwt', () => {
-          it('should call the success handler with true', done => {
-            jwtIsValid = true;
-            var origJwt = authHelper.jwt;
-            authHelper.jwt = jest.fn();
-            authHelper.jwt.mockImplementation(() => {
-              return jwtToken;
-            });
-            authHelper.login().then(
-              handleSuccess,
-              handleFailure
-            );
-            authHelper.jwt = origJwt;
-            setImmediate(() => {
-              expect(handleSuccess).toBeCalledWith(
-                true
-              );
-              expect(authClient.authenticate).toBeCalledWith(
-                expectedCode,
-                window.location.origin+window.location.pathname,
-                expect.anything(),
-                expect.anything()
-              )
-              done();
-            });
-          });
-
-          it('should store the jwtToken and expiration in the sessionStorage', done => {
-            jwtIsValid = true;
-            authHelper.login().then(
-              handleSuccess,
-              handleFailure
-            );
-            setImmediate(() => {
-              expect(sessionStorage.setItem).toHaveBeenCalledWith(expectedJwtStoreKey, jwtToken);
-              expect(sessionStorage.setItem).toHaveBeenCalledWith(expectedJwtExpirationStoreKey, expect.any(Number));
-              done();
-            });
-          });
-        });
-        describe('and is not a valid jwt', () => {
-          it('should call the failureHandler with the error message', done => {
-            jwtIsValid = false;
-            let expectedFailureMessage = `Could not get JwtToken: ${invalidTokenMessage}`;
-            authHelper.login().then(
-              handleSuccess,
-              handleFailure
-            );
-            setImmediate(() => {
-              expect(handleFailure).toBeCalledWith(
-                expectedFailureMessage
-              );
-              done();
-            });
-          });
-        });
-      });
-      describe('access code does not exist', () => {
-        expectedCode = undefined;
-        beforeEach(() => {
-          codeExists = false;
-        });
-
-        it('should assign the window location to the oauth url', done => {
-          authHelper.login().then(
-            handleSuccess,
-            handleFailure
+    describe('called with an invalid code', () => {
+      it('should call the failureHandler with the error message', done => {
+        jwtIsValid = false;
+        let expectedFailureMessage = `Could not get JwtToken: ${invalidTokenMessage}`;
+        authHelper.login().then(
+          handleSuccess,
+          handleFailure
+        );
+        setImmediate(() => {
+          expect(handleFailure).toBeCalledWith(
+            expectedFailureMessage
           );
-          setImmediate(() => {
-            expect(window.location.assign).toBeCalledWith(mockedOauthRedirectUrl);
-            expect(handleSuccess).toBeCalledWith(
-              true
-            );
-            done();
-          });
+          done();
         });
       });
     });
